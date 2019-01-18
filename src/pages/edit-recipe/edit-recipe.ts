32b561b46a311579cf4426b24ac23d122d3b6218
ciro <cirux88@gmail.com>
2019-01-18 08:40:31 +0100

    form with select input

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,11 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the EditRecipePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
+ * 
  */
 
 @IonicPage()
@@ -13,9 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage {
+export class EditRecipePage implements OnInit {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  mode = 'New';
+  selectOptions = ['Easy', 'Medium', 'Hard'];
+
+  constructor(public navCtrl: NavController, private navParams: NavParams) {
+  }
+
+  ngOnInit(){
+    this.mode = this.navParams.get('mode');
   }
 
   ionViewDidLoad() {
