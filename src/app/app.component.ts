c7a0cb48c8b7c9c2ff790454d3a308c90958957e
ciro <cirux88@gmail.com>
2019-02-04 08:49:46 +0100

    new pages for signing and signup, sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,45 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
+import { SigninPage } from '../pages/signin/signin';
+import { SignupPage } from '../pages/signup/signup';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyAppRecipeBook {
-  rootPage:any = TabsPage;
+  // available root pages
+  tabsPage:any = TabsPage;
+  signinPage:any = SigninPage;
+  signupPage:any = SignupPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  // gets a reference to the ion-nav component, using the local reference 'nav'
+  @ViewChild('nav') nav: NavController;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
+    private menuCtrl: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  /**
+   * Set the input page as root
+   * @param page the page to be set as root
+   */
+  onLoad(page: any){
+    this.nav.setRoot(page);
+    // close the menu
+    this.menuCtrl.close();
+  }
+
+  onLogout(){
+    // TODO
   }
 }
 
